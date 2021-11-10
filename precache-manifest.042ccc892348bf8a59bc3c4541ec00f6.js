726f8ef17cc2289bf548c139bcddcf6922008946
AACGiron <alexanderangelo.giron.iics@ust.edu.ph>
2021-11-10 14:10:14 +0800

    Updates

---
+++ precache-manifest.042ccc892348bf8a59bc3c4541ec00f6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66489719df0477f599b550d9fa82a01c",
+    "url": "/myfirstreact/index.html"
+  },
+  {
+    "revision": "10f878257221739c54af",
+    "url": "/myfirstreact/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "df57452eb84c9e284114",
+    "url": "/myfirstreact/static/js/2.c0605a72.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/myfirstreact/static/js/2.c0605a72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77b78f95956a20060263",
+    "url": "/myfirstreact/static/js/3.9cac2aa4.chunk.js"
+  },
+  {
+    "revision": "10f878257221739c54af",
+    "url": "/myfirstreact/static/js/main.b04c5940.chunk.js"
+  },
+  {
+    "revision": "f934006b073248f5f532",
+    "url": "/myfirstreact/static/js/runtime-main.d2fa4f2e.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/myfirstreact/static/media/logo.06e73328.svg"
+  }
+]);
